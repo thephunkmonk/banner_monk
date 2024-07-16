91c021a1df08d70cac3973fe0a81576e7f3ff153
thephunkmonk <andrewchoi1218@gmail.com>
2024-07-16 14:36:24 +0900

    added some to README.md

--- src/banner_monk/banner_monk.py
+++ src/banner_monk/banner_monk.py
@@ -1,5 +1,47 @@
 from pyfiglet import Figlet
+import random
 
 def show():
     f = Figlet(font='slant')
     print(f.renderText("it's ya boiiiiii"))
+
+def pic():
+    p = """
+
+                                      '$&
+                                      @R$k
+                                    '$!!!M&
+                                    @?~~~!$k
+                                  '9!!~ ~~!MX
+                                  @X~~   `~!$k
+                                 9!!      ~~!$X
+                                dR!~       `~!$>
+                               XR!~         `~!$k:
+                              tR!~        ::~~!!MMXXHHHH!!<:.
+                             <$!xxiXX!!!~~~~~~~!!MMMMMMMMMMMMMMMXXXXx::
+                        .:X@N$$$RMMX!!!!~~~~~~~~!!MMMM@@MMMMMMMMMMMMXMSMMtHHHX!
+                  :xiM#"~  <$!~  `~~~!~~~~~~~~~~~!!MX!!!!!??#RR888MMMMMMMMMMMHH
+           ..XH@!~`       X$!~                 '~~~!MX!!!!!!!!!!!!?MMR@@$MMMMMM
+     :xiM#"~             <$!~                     '~!MM??MMX!!!!!!!!!!!!!!??#R$
+XH@M!``                 :$!!                        ~!R:   `~!MM!XH!!!!!!!!!!!!
+                       <$!~                         `!!M:        `~"??HHX!!!!!!
+                      :$!~                           `!!R:              `!!MMMH
+                     '$!~                             `~!8:                   ~
+                    :$!!                               ~!!N:
+                   '$!~                                 `!!N:
+                  .$!!~                                  ~~!&>
+                 '@!!~                                    ~!MN
+                .$!!~                                     ~~!M&>
+               :@$MHHHHHHHHH!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!H$N
+                 PINK FLOYD:  DARK SIDE OF THE MOON  BY CHAT 95
+
+
+       """
+
+    print(p)
+
+
+def lotto():
+    l = random.sample(range(1,46),6)
+    print(l)
+   
